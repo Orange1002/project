d49ea90c970825954a90ad682e57db09feb9fcc6
Orange1002 <f129153@gmail.com>
2025-05-15 14:04:24 +0800

    Nav Link接好

--- next/app/shopcart/page.js
+++ next/app/shopcart/page.js
@@ -5,13 +5,7 @@ import React, { useState, useEffect } from 'react'
 export default function ShopcartPage(props) {
   return (
     <>
-      <section>
-        <div className="container">
-          <div className="d-flex justify-content-center">
-            <div className="fs-36 mt-5">購物車</div>
-          </div>
-        </div>
-      </section>
+      <div>購物車</div>
     </>
   )
 }
